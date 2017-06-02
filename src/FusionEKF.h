814378d64e07870d136fc86d6224a6cf2361f55a
Oleg Potkin <olpotkin@gmail.com>
2017-06-01 19:11:39 -0700

    Add private parameters

--- src/FusionEKF.h
+++ src/FusionEKF.h
@@ -38,6 +38,9 @@ private:
   // previous timestamp
   long long previous_timestamp_;
 
+  int noise_ax;
+  int noise_ay;
+
   // tool object used to compute Jacobian and RMSE
   Tools tools;
   Eigen::MatrixXd R_laser_;
