ad23b7fa3bd14d187cec41c2812a0229a92587b3
olpotkin <olpotkin@gmail.com>
2020-10-05 13:58:48 +0200

    feat: Apply styleguide to tools

--- src/tools.cpp
+++ src/tools.cpp
@@ -1,16 +1,20 @@
 #include <iostream>
 #include "tools.h"
 
+
 using Eigen::VectorXd;
 using Eigen::MatrixXd;
 using std::vector;
+
 
 Tools::Tools() {}
 
 Tools::~Tools() {}
 
-VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
-                              const vector<VectorXd> &ground_truth) {
+VectorXd Tools::CalculateRMSE(
+  const vector<VectorXd>& estimations,
+  const vector<VectorXd>& ground_truth)
+{
   // Calculate the RMSE
   VectorXd rmse(4);
   rmse << 0, 0, 0, 0;
@@ -26,6 +30,7 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
   // Accumulate squared residuals
   for (unsigned int i = 0; i < estimations.size(); ++i) {
     VectorXd residual = estimations[i] - ground_truth[i];
+    
     // Coefficient-wise multiplication
     residual = residual.array() * residual.array();
     rmse += residual;
